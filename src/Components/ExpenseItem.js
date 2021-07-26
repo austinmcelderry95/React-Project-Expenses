877dd65e2169dce950935b2ce99b0f5723b5753b
austinmcelderry95 <austinmcelderry@gmail.com>
2021-07-26 11:58:51 -0600

    adding project

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,15 +1,21 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 7, 26);
-  
-    return (
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
+  return (
     <div className="expense-item">
-      <div>July 26th 2021</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-      <div className="expense-item__price">$100.60</div>
-    </div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
     </div>
   );
 }
